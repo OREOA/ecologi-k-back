14b62143b96225c7114ff5380f45ebef6856cc6d
Otto Hyytiälä <otto.hyytiala@gmail.com>
2018-11-24 13:46:58 +0200

    things

--- controllers/example.js
+++ controllers/example.js
@@ -1,7 +1,29 @@
 const exampleRouter = require('express').Router()
+var fs = require('fs');
+var parse = require('csv-parse');
 
 exampleRouter.get('/', async (req, res) => {
-  res.send('Hello world')
+  var csvData=[];
+  fs.createReadStream('./data.csv')
+    .pipe(parse({
+      delimiter: ':',
+      from_line: 2
+    }))
+    .on('data', function(csvrow) {
+      console.log(csvrow)
+      //do something with csvrow
+      var row = csvrow[0].split(',')
+      csvData.push({
+        EAN: row[0],
+        Quantity: row[1],
+        userId: row[2],
+      });
+    })
+    .on('end',function() {
+      //do something wiht csvData
+      console.log('done');
+      res.send(csvData)
+    });
 })
 
 module.exports = exampleRouter
