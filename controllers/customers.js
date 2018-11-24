4a675f07cfd70eeee7f64ce47eb76d17d6e0bb91
Otto Hyytiälä <otto.hyytiala@gmail.com>
2018-11-24 18:05:45 +0200

    customers

---
+++ controllers/customers.js
@@ -0,0 +1,26 @@
+const customerRouter = require('express').Router()
+const Customer = require('../models/customer')
+
+customerRouter.get('/', async (request, response) => {
+  const customers = await Customer
+    .find({})
+  response.json(customers.map(Customer.format))
+})
+
+customerRouter.post('/', async (request, response) => {
+  try {
+    const body = request.body
+    const customer = new Customer({
+      PersonAgeGrp: body.personAgeGrp,
+      KCustomer: body.kCustomer
+    })
+    const savedCustomer = await customer.save()
+
+    response.json(Customer.format(savedCustomer))
+  } catch (exception) {
+    console.log(exception)
+    response.status(500).json({ error: 'something went wrong...' })
+  }
+})
+
+module.exports = customerRouter
---
+++ models/customer.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const customerSchema = new mongoose.Schema({
+  PersonAgeGrp: String,
+  KCustomer: String
+})
+
+customerSchema.statics.format = (customer) => {
+  return {
+    id: customer.id,
+    PersonAgeGrp: customer.PersonAgeGrp,
+    KCustomer: customer.KCustomer
+  }
+}
+
+const Customer = mongoose.model('Customer', customerSchema)
+
+module.exports = Customer
